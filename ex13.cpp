559dba0037df709978cdb3f2df309bfb1826af4b
MouadKH9 <mouad.khchich@gmail.com>
2019-11-06 11:05:45 +0100

    Finished first 2 exercices

---
+++ ex13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int n, i, j;
+    cout << "Entrer le nombre des lignes: \n";
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - i; j++)
+            cout << " ";
+        for (j = -i; j <= i; j++)
+            cout << "*";
+        cout << "\n";
+    }
+}
